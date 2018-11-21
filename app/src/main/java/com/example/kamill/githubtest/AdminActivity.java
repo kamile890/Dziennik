7664a2e1ea2708f58063672a351752df9817a6d7
Kamil <kamil.klosek2@gmail.com>
2018-11-21 11:14:18 +0100

    Dodanie aktywności "Dodawanie_ucznia" do panelu admina

--- app/src/main/java/com/example/kamill/githubtest/AdminActivity.java
+++ app/src/main/java/com/example/kamill/githubtest/AdminActivity.java
@@ -1,7 +1,9 @@
 package com.example.kamill.githubtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
     }
+
+    public void nowy_uczen(View v){
+        Intent intent = new Intent(this, Dodawanie_ucznia.class);
+        startActivity(intent);
+    }
+
+
 }
