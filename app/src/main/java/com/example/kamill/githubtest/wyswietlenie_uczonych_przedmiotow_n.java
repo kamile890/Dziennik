4eaabae6075eb2d4c893975e6d13d0cfaf1be7b6
irek140 <irek140@poczta.fm>
2018-12-14 21:48:09 +0100

    dodanie fragmentow i klas java //do nauczyciela

--- app/src/main/java/com/example/kamill/githubtest/wyswietlenie_uczonych_przedmiotow_n.java
+++ app/src/main/java/com/example/kamill/githubtest/wyswietlenie_uczonych_przedmiotow_n.java
@@ -1,6 +1,24 @@
 package com.example.kamill.githubtest;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class wyswietlenie_uczonych_przedmiotow_n extends Fragment {
+
+    public wyswietlenie_uczonych_przedmiotow_n() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_wyswietlenie_uczonych_przedmiotow_n, container, false);
+
+
+        return v;
+    }
 }
